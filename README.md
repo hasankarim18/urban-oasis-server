5d5b9251bd348300f6209d5e18bd032065445dd9
hasankarim18 <hallysust@gmail.com>
2023-05-15 00:20:32 +0600

    successfully connected to mongodb atlas

---
+++ README.md
@@ -0,0 +1,11 @@
+# Urban Oasis (ema-jhon-simple)
+
+- firebase:: id: hallysust
+- firebase project name:: urban-oasis
+- ### [`Project Live Link firebase`](https://urban-orasis.web.app)
+- ### [`Project github repo Link`](https://github.com/hasankarim18/urban-oasis)
+- ### [`Project server github repo Link`](https://github.com/hasankarim18/urban-oasis-server)
+
+
+### Live Link surge: http://delightful-growth.surge.sh/
+### Live Link Netlify: https://simpleecommerce18.netlify.app/
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express()
+require("dotenv").config();
 const port = process.env.PORT || 5000 
 
 // middleware 
@@ -8,8 +10,45 @@ app.use(cors())
 app.use(express.json()) 
 
 
+// env vaiable 
+const db_user = process.env.DB_USER;
+const db_password = process.env.DB_PASSWORD;
+
+
+
+const uri =
+  `mongodb+srv://${db_user}:${db_password}@cluster0.nzfxe6e.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
 app.get('/', (req, res)=> {
-    res.send('Enjoy your urban expending and make us rich')
+    res.send(`Enjoy your urban expending and make us rich `);
 } )
 
 app.listen(port, ()=> {
